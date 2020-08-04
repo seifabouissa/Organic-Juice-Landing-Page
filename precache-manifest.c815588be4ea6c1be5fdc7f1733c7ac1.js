beafa79950887f840b87a450923f14e2471f9000
seifabouissa <seifeldinashrafabouissa@gmail.com>
2020-08-04 23:13:54 +0200

    Updates

---
+++ precache-manifest.c815588be4ea6c1be5fdc7f1733c7ac1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92a5a521bed056fb159b0f779c59402f",
+    "url": "/Organic-Juice-Landing-Page/index.html"
+  },
+  {
+    "revision": "808dbed024a3607338fd",
+    "url": "/Organic-Juice-Landing-Page/static/css/main.e7d59a88.chunk.css"
+  },
+  {
+    "revision": "8fd32fd45f7b3b6894dc",
+    "url": "/Organic-Juice-Landing-Page/static/js/2.1541a7a8.chunk.js"
+  },
+  {
+    "revision": "c344ff0e273d3dd0833ad3a0d4c0857e",
+    "url": "/Organic-Juice-Landing-Page/static/js/2.1541a7a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "808dbed024a3607338fd",
+    "url": "/Organic-Juice-Landing-Page/static/js/main.4c5b6417.chunk.js"
+  },
+  {
+    "revision": "6a4624933f5c7b673520",
+    "url": "/Organic-Juice-Landing-Page/static/js/runtime-main.44cfc5c4.js"
+  },
+  {
+    "revision": "8df76c6d7e8a76d346481fa0e25fe13e",
+    "url": "/Organic-Juice-Landing-Page/static/media/juice.8df76c6d.png"
+  },
+  {
+    "revision": "0862df3ae93e5aadfa305d3cbaed32c8",
+    "url": "/Organic-Juice-Landing-Page/static/media/leaf01.0862df3a.png"
+  },
+  {
+    "revision": "0be40e93f51b6be89cbde57a52d04b52",
+    "url": "/Organic-Juice-Landing-Page/static/media/leaf02.0be40e93.png"
+  },
+  {
+    "revision": "221dcc7e69cb479bed79ac29ee7b9204",
+    "url": "/Organic-Juice-Landing-Page/static/media/leaf03.221dcc7e.png"
+  },
+  {
+    "revision": "8a9a03fe60f4f2a3694f30ad95e83f98",
+    "url": "/Organic-Juice-Landing-Page/static/media/leaf05.8a9a03fe.png"
+  }
+]);
